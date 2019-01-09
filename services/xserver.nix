3419ed79545f5a34bd07f78638baacd76ae88a7b
jonlaokan <oneofdays@gmail.com>
2019-01-09 14:31:55 +0100

    refactor(xserver): apply minor changes

--- services/xserver.nix
+++ services/xserver.nix
@@ -1,41 +1,37 @@
 { pkgs, ...}:
 
 {
-
   # Enable the X11 windowing system.
-   services.xserver.enable = true;
-   services.xserver.autorun = true;
-   services.xserver.layout = "us";
-   services.xserver.xkbVariant = "colemak";
-   services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable touchpad support.
-  # services.xserver.libinput.enable = true;
-  security.sudo.enable = true;
-
-  services.xserver.windowManager.xmonad = {
+  services.xserver = {
     enable = true;
-    enableContribAndExtras = true;
-    extraPackages = haskellPackages: [
-    	haskellPackages.xmonad-contrib
-	haskellPackages.xmonad-extras
-	haskellPackages.xmonad
-    ];
-  };
- 
-  # Use lightdm
-  services.xserver.displayManager.lightdm = {
-	  enable = true;
-	  greeters.gtk.indicators = [ "~host" "~spacer" "~spacer" "~a11y" "~session" "~power"];
+    autorun = true;
+    layout = "us";
+    xkbVariant = "colemak";
+    xkbOptions = "eurosign:e";
+
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      extraPackages = haskellPackages: [
+        haskellPackages.xmonad-contrib
+        haskellPackages.xmonad-extras
+        haskellPackages.xmonad
+      ];
+    };
+
+    displayManager.lightdm = {
+      enable = true;
+      greeters.gtk.indicators = [ "~host" "~spacer" "~spacer" "~a11y" "~session" "~power"];
+    };
   };
 
-  fonts = {
-    enableFontDir = true;
-	fonts = with pkgs; [
-	  #nerdfonts
-	  opensans-ttf
-	  siji
-	];
+  services.redshift = {
+    enable = true; 
+    latitude = "43.53";
+    longitude = "1.46";
+    temperature = {
+      day = 5500;
+      night = 3500;
+    };
   };
-
 }
